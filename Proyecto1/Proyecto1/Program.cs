62acd5e01133c5ad6fc042747eb36119af15d279
YonathanAcosta <yonathan.acosta@ithealth.co>
2022-10-04 15:53:57 -0500

    Se le añade FOR

--- Proyecto1/Proyecto1/Program.cs
+++ Proyecto1/Proyecto1/Program.cs
@@ -2,7 +2,15 @@
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("Digite el numero de notas");
         int num1 = int.Parse(Console.ReadLine());
         Console.WriteLine("El numero es: "+num1);
+        //Prueba del promedio
+        int[] numeros=new int[num1] ;
+        for(int i = 0; i < num1; i++){
+            Console.WriteLine("Escriba la nota " + i + 1);
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+        
     }
 }
