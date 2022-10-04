71bc5b867ed5a1963be2152b0fe9dd3a11dc6e2f
YonathanAcosta <yonathan.acosta@ithealth.co>
2022-10-04 15:32:19 -0500

    Añaddir Ficheros

---
+++ Proyecto1/Proyecto1/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("El numero es: "+num1);
+    }
+}
